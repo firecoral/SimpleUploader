9f7520ccd2a005ad2257a35dc8ffe4d803826393
Chris Guthrie <cguthrie@digiproofs.com>
2016-11-01 14:33:10 -0700

    merging

--- JSONUploaderTest/UnitTest1.cs
+++ JSONUploaderTest/UnitTest1.cs
@@ -3,7 +3,6 @@ using System.Diagnostics;
 using System.Threading.Tasks;
 using DigiProofs.JSONUploader;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-using Newtonsoft.Json;
 
 namespace JSONUploaderTest
 {
